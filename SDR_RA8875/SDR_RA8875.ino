284436dfeb5ef4d0b949924d05620de1a2d8ae9a
K7MDL2 <64443004+K7MDL2@users.noreply.github.com>
2022-12-11 19:05:53 -0800

    Changed default sample rate to 96K

--- SDR_RA8875/SDR_RA8875.ino
+++ SDR_RA8875/SDR_RA8875.ino
@@ -209,8 +209,8 @@ bool    MF_default_is_active = true;
 //float sample_rate_Hz = 11000.0f;  //43Hz /bin  5K spectrum
 //float sample_rate_Hz = 22000.0f;  //21Hz /bin 6K wide
 //float sample_rate_Hz = 44100.0f;  //43Hz /bin  12.5K spectrum
-float sample_rate_Hz = 48000.0f;  //46.875Hz /bin  24K spectrum for 1024.  
-//float sample_rate_Hz = 96000.0f;  // <100Hz/bin at 1024FFT, 50Hz at 2048, 40Khz span at 800 pixels and 2048FFT
+//float sample_rate_Hz = 48000.0f;  //46.875Hz /bin  24K spectrum for 1024.  
+float sample_rate_Hz = 96000.0f;  // <100Hz/bin at 1024FFT, 50Hz at 2048, 40Khz span at 800 pixels and 2048FFT
 //float sample_rate_Hz = 192000.0f; // 190Hz/bin - does
 
 float zoom_in_sample_rate_Hz = sample_rate_Hz;  // used in combo with new fft size for zoom level
