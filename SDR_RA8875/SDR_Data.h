883f59190fa8de3239bf944240d0cf17e6ce20b1
K7MDL2 <64443004+K7MDL2@users.noreply.github.com>
2022-12-22 02:40:45 -0800

    TXLVL chg, BTNs reassigned, ENCx_BTN now SWx_BTN

--- SDR_RA8875/SDR_Data.h
+++ SDR_RA8875/SDR_Data.h
@@ -301,7 +301,7 @@ struct Label labels[LABEL_NUM] = {
 struct User_Settings user_settings[USER_SETTINGS_NUM] = {                      
 //Profile name    sp_preset mn  sub_VFO  sv_md uc1 uc2 uc3  lastB   mute  mic_En  micG LInLvl rfg_en rfGain SpkEn afgen afGain LoRX LoTX enet  enout  nben   nblvl  nren  spot  rbeep pitch   notch  xmit fine VFO-AB Zoom_lvl panEn panlvl
     {"ENET ON Config",    0, 0, 28000000, USB, 0,  0,  0, BAND80M,   OFF, MIC_ON,  76.0,  15,   OFF,   100,   ON,   OFF, 100,  16,  16,   ON,  OFF,  OFF,  NBOFF,  OFF,  OFF,  0.02,  600, NTCHOFF, OFF, OFF,   0,    ZOOMx1, OFF, 50},
-    {"User Config #2",    0, 0, 14200000, USB, 0,  0,  0, BAND30M,   OFF, MIC_ON,  50.0,  15,   OFF,   100,   ON,   OFF, 10,   15,  14,  OFF,  OFF,  OFF,  NBOFF,  OFF,  OFF,  0.02,  600, NTCHOFF, OFF, OFF,   0,    ZOOMx1, OFF, 50},
+    {"User Config #2",    0, 0, 14200000, USB, 0,  0,  0, BAND30M,   OFF, MIC_ON,  50.0,  15,   OFF,   100,   ON,   OFF, 10,   15,  18,  OFF,  OFF,  OFF,  NBOFF,  OFF,  OFF,  0.02,  600, NTCHOFF, OFF, OFF,   0,    ZOOMx1, OFF, 50},
     {"PanAdapter Config", 0, 0, 1420000,  USB, 0,  0,  0, PAN_ADAPT, OFF, MIC_OFF, 76.0,  15,   OFF,   100,   ON,   OFF, 100,  16,  16,  OFF,  OFF,  OFF,  NBOFF,  OFF,  OFF,  0.02,  600, NTCHOFF, OFF, OFF,   0,    ZOOMx1, OFF, 50},
 };
 
@@ -321,12 +321,12 @@ struct User_Settings user_settings[USER_SETTINGS_NUM] = {
 struct EncoderList encoder_list[NUM_AUX_ENCODERS] {
 //type          id    enabled            def_MF   enca         a_active    encb            enc1_tap        enc1_press
     {GPIO_ENC,  0,    GPIO_VFO_ENABLE,   NONE,    NONE,        NONE,       NONE,           NONE,           NONE},      // enc slot 1 of 6
-    {GPIO_ENC,  1,    GPIO_ENC2_ENABLE,  MFTUNE,  MFTUNE,      ON,         MODE_BTN,       ENC1_BTN,       FINE_BTN},      // enc slot 1 of 6
-    {I2C_ENC,   2,    I2C_ENC1_ENABLE,   NONE,    MFTUNE,      ON,         RATE_BTN,       ENC2_BTN,       FINE_BTN},    // enc slot 2
-    {I2C_ENC,   3,    I2C_ENC2_ENABLE,   NONE,    ZOOM_BTN,    ON,         PAN_BTN,        ENC3_BTN,       RIT_BTN},      // enc slot 3
-    {I2C_ENC,   4,    I2C_ENC3_ENABLE,   NONE,    AFGAIN_BTN,  ON,         RFGAIN_BTN,     ENC4_BTN,       BAND_BTN},      // enc slot 4
-    {I2C_ENC,   5,    I2C_ENC4_ENABLE,   NONE,    RIT_BTN,     ON,         XIT_BTN,        ENC5_BTN,       RIT_BTN},       // enc slot 5
-    {I2C_ENC,   6,    I2C_ENC5_ENABLE,   NONE,    NB_BTN,      ON,         REFLVL_BTN,     BANDUP_BTN,     BANDDN_BTN}     // enc slot 6
+    {GPIO_ENC,  1,    GPIO_ENC2_ENABLE,  MFTUNE,  MFTUNE,      ON,         RATE_BTN,       SW1_BTN,        FINE_BTN},      // enc slot 1 of 6
+    {I2C_ENC,   2,    I2C_ENC1_ENABLE,   NONE,    AFGAIN_BTN,  ON,         RFGAIN_BTN,     SW2_BTN,        FINE_BTN},    // enc slot 2
+    {I2C_ENC,   3,    I2C_ENC2_ENABLE,   NONE,    ZOOM_BTN,    ON,         PAN_BTN,        SW3_BTN,        RIT_BTN},      // enc slot 3
+    {I2C_ENC,   4,    I2C_ENC3_ENABLE,   NONE,    FILTER_BTN,  ON,         MODE_BTN,       SW4_BTN,        BAND_BTN},      // enc slot 4
+    {I2C_ENC,   5,    I2C_ENC4_ENABLE,   NONE,    RIT_BTN,     ON,         XIT_BTN,        SW5_BTN,        RIT_BTN},       // enc slot 5
+    {GPIO_SW,   6,    GPIO_SW1_ENABLE,   NONE,    NONE,        ON,         NONE,           BANDUP_BTN,     BANDDN_BTN}     // enc slot 6
 };
 
 struct Frequency_Display disp_Freq[FREQ_DISP_NUM] = {
